2e53caf15cf58d302ddb6e65ea81d548c7e5818c
Md. Tanvirul Islam <md.tanvirulislam@outlook.com>
2022-10-03 14:14:25 +0600

    address related request

---
+++ app/Http/Requests/AddressUpdateRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class AddressUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'ip'    => ['required', 'string', Rule::unique('addresses')->ignore($this->address->id)],
+            'label' => ['required', 'string', Rule::unique('addresses')->ignore($this->address->id)]
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(errorResponse(422, $validator->errors(), 'Validation Error.'));
+    }
+}
